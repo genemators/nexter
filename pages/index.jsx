d93de0ff560b5947df6cc040ef0d00bede6477a1
Geno Ferollo <genemators@gmail.com>
2020-11-18 18:57:53 +0500

    stats

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,101 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import Head from "next/head";
 import axios from "axios";
 
-export default function Home() {
-	const [userData, setSetUserData] = useState({
+export default class Home extends Component() {
+	constructor(props, context) {
+		super(props, context);
+
+		this.state = {
 			firstname: "",
 			lastname: "",
 			passport_data: "",
 			birthday: "",
 			location_lat: "",
 			location_long: "",
-			image_file: "",
-			user_image_file: "",
-		}),
-		handleChange = (e) => {
-			setSetUserData([(e.target.name = e.target.value)]);
 		};
-
-	const handleSubmit = (e) => {
+	}
+	onChange(e) {
+		this.setState({
+			firstname: e.target.value,
+			lastname: e.target.value,
+			passport_data: e.target.value,
+		});
+	}
+	onSubmit(e) {
 		e.preventDefault();
-		console.log(userData.passport_data);
-		let url = "http://dd913f614b33.ngrok.io/api/postuser";
-		axios
-			.post(url, FormData)
-			.then((res) => {
-				console.log(res.data);
-			})
-			.catch((err) => console.log(err));
-	};
 
-	return (
-		<>
-			<Head>
-				<title>VTracker</title>
-				<meta property="og:title" content="VTracker" />
-				<meta
-					property="og:description"
-					content="Siz bla bla bla... VTracker"
-				/>
-			</Head>
-			<div className="App">
-				<div>
-					<form onSubmit={handleSubmit}>
-						<label>Img</label>
-						<br />
-						<input
-							name="firstname"
-							type="text"
-							onChange={handleChange}
-						/>
-						<input
-							name="lastname"
-							type="text"
-							onChange={handleChange}
-						/>
-						<input
-							name="passport_data"
-							type="text"
-							onChange={handleChange}
-						/>
-						<input
-							name="birthday"
-							type="text"
-							onChange={handleChange}
-						/>
-						<input
-							name="location_lat"
-							type="number"
-							onChange={handleChange}
-						/>
-						<input
-							name="location_long"
-							type="number"
-							onChange={handleChange}
-						/>
-						<input
-							name="image_file"
-							type="file"
-							multiple
-							onChange={handleChange}
-						/>
-						<input
-							name="user_image_file"
-							type="file"
-							multiple
-							onChange={handleChange}
-						/>
-						<br />
-						<button>Submit</button>
-					</form>
+		fetch(this.form.formAction, {
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				firstname: this.state.firstname,
+				lastname: this.state.lastname,
+				passport_data: this.state.passport_data,
+				birthday: this.state.birthday,
+				location_lat: this.state.location_lat,
+				location_long: this.state.location_long,
+			}),
+		}).then((r) => console.log(r));
+
+		this.setState({
+			firstname: "",
+			lastname: "",
+			passport_data: "",
+			birthday: "",
+			location_lat: "",
+			location_long: "",
+		});
+	}
+	render() {
+		return (
+			<>
+				<Head>
+					<title>VTracker</title>
+					<meta property="og:title" content="VTracker" />
+					<meta
+						property="og:description"
+						content="Siz bla bla bla... VTracker"
+					/>
+				</Head>
+				<div className="App">
+					<div>
+						<form onSubmit={handleSubmit}>
+							<input
+								name="firstname"
+								type="text"
+								onChange={handleChange}
+							/>
+							<input
+								name="lastname"
+								type="text"
+								onChange={handleChange}
+							/>
+							<input
+								name="passport_data"
+								type="text"
+								onChange={handleChange}
+							/>
+							<input
+								name="birthday"
+								type="text"
+								onChange={handleChange}
+							/>
+							<input
+								name="location_lat"
+								type="number"
+								onChange={handleChange}
+							/>
+							<input
+								name="location_long"
+								type="number"
+								onChange={handleChange}
+							/>
+							<br />
+							<button>Submit</button>
+						</form>
+					</div>
 				</div>
-				{userData.image_file ? (
-					<img src={userData.image_file} />
-				) : (
-					<p> Img not found </p>
-				)}
-			</div>
-		</>
-	);
+			</>
+		);
+	}
 }
